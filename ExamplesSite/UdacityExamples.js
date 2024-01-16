7f7319aaaced21d01f654bef19c5b1c2b643a8ac
JanvL <johnoflion@gmail.com>
2024-01-16 23:37:33 +0100

    jsDate

--- ExamplesSite/UdacityExamples.js
+++ ExamplesSite/UdacityExamples.js
@@ -13,6 +13,12 @@ if (typeof document !== "undefined") {
 // Your code here
 let showDate = document.getElementById("resentDate");
 const d = new Date();
+let year = d.getFullYear();
+const yearMonth = ["Januari", "Februari", "Maart", "April", "Mei", "Juni", "Juli", "Augustus", "September", "Oktober", "November", "December"]
+let month = yearMonth[d.getMonth()];
+const weekday = ["Zondag", "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"];
+let day = weekday[d.getDay()];
+let dayNr = d.getDate();
 console.log(d);
-showDate.innerHTML = d;
+showDate.innerHTML = `${day} ${dayNr} ${month} ${year}`;
 //});
